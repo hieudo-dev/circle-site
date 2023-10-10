a4ae4529a15912237e66f6405348a3705bed2a88
Hieu Do <hieudo1@beeketing.net>
2023-10-10 21:37:13 +0700

    Updates

--- index.2d4f65de.js
+++
@@ -1,3 +0,0 @@
-const e=document.querySelectorAll(".page");for(let t=0;t<e.length;t++){let o=new IntersectionObserver(e=>{e.forEach(e=>{e.intersectionRatio>.1&&e.target.classList.remove("page--out-of-view")})},{threshold:.1});o.observe(e[t])}//# sourceMappingURL=index.2d4f65de.js.map
-
-//# sourceMappingURL=index.2d4f65de.js.map
